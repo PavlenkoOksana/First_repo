58c793580f04fb2cb7aad7261cc2316aef600c75
PavlenkoOksana <pavlenko.oksana.od@gmail.com>
2023-09-12 13:11:17 +0200

    12/09/2023

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
-num1=int(input ("enter the first number "))
-num2=int(input("enter second number "))
-num1 +=5
-print ("Result +: ", num1+num2)
-print ("Result -: ", num1-num2)
-print ("Result *: ", num1*num2)
-print ("Result /: ", num1/num2)
-print ("Result **: ", num1**num2)
-print ("Result //: ", num1//num2)
-word = "Hi"
-print(word*2)
+#count = 0
+#word = "Hello World!"
+#for i in word:
+#  if i=="w":
+#    count +=1
+#print (count)
+
+#isHasCar = True
+#while isHasCar:
+#  if input("Enter data: ") == "Stop":
+#    isHasCar=False
+#    print ("End of cycle")
+#  else:
+#    print("cycle go on")
+
+#for i in range(1,11):
+#    if i==5:
+#      break
+#    if i%2==00:
+#      continue
+#    print(i)  
+
+found = None
+for i in "hello":
+  if i=="l":
+    found = True
+    break
+  else:
+    found = False
+print(found)
