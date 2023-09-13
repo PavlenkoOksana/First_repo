35bffe7c2a21e3b872274f6c99952e5e8d8a57ca
PavlenkoOksana <pavlenko.oksana.od@gmail.com>
2023-09-13 13:17:57 +0200

    13/09/2023

--- main.py
+++ main.py
@@ -20,11 +20,81 @@
 #      continue
 #    print(i)  
 
-found = None
-for i in "hello":
-  if i=="l":
-    found = True
-    break
-  else:
-    found = False
-print(found)
+#found = None
+#for i in "hello":
+#  if i=="l":
+#    found = True
+#    break
+#  else:
+#    found = False
+#print(found)
+
+#LIST
+#nums = [5, 7, 2, 4, 7, True, "Hello", 6.7, [5, 7]]
+#nums[0] = 50
+#nums[5]=777
+#print(nums[-1][1]) #[-1]обратный индекс, обращение к последнему элементу списка
+
+#numbers = [5, 2, 7]
+#numbers.append(100) #добавляет новый элемент в конец списка
+#print(numbers)
+#numbers.insert(1, True) #вставляет элемент на заданное место в списке, остальные элементы смещаются
+#print(numbers)
+#numbers.extend([9, 9, 9]) #вставляет новый список в конец существующего
+#print(numbers)
+#b = [0, 0, 0]
+#numbers.extend(b)
+#print(numbers)
+#numbers.sort()
+#print (numbers)
+#numbers.reverse() # реверсирует элементы в списке
+#print(numbers)
+#numbers.pop (-7) #удаляет последний элемент, если значение не задано, если передать значение, то удалить элемент по индексу, при этом можно использовать отрицат индексы
+#print(numbers)
+#numbers.remove(True)
+#numbers.clear () #удаляет элементы списка
+#print(numbers)
+#print(numbers.count(2)) #считает количество элементов что совпадают со значением, что передано в метод
+#print(len(numbers)) #выводит длину списка
+
+#nums = [5, 2, 7, "50", False ]
+#i=0
+#for el in nums:
+# el*=2
+# print (el)
+# nums[i]=el
+# i+=1
+#print(nums)
+
+#n=int(input("Enter length: "))
+#user_list=[]
+#i=0
+#while i < n:
+#  string = "Enter element #" + str(i+1) + ": "
+#  user_list.append(input(string))
+#  i+=1 
+#print (user_list)
+
+#STRING
+word = 'fooTThball, wollEYball, tEnnis'
+#print(word)
+#print (len(word))
+#print(word.count('r'))
+#print(word.upper())
+#print(word.isupper())
+#print(word.islower())
+#print(word.capitalize())
+#print(word.find('y')) #ищет символ и возвращает его индекс
+#print(word.split(',')) #разбивает строку на список из элементов по отределенному символу
+#переводит строку в список, потом привод каждый элемент списка к правилу каждый элемент с большой буквы, остальные маленькие, затем объединяет список в строку с разделениями ,
+#hobby=word.split(', ')
+#for i in range(len(hobby)):
+#  hobby[i]=hobby[i].capitalize()
+#print(hobby)
+#result =', '.join(hobby)
+#print(result)
+#-------------------- end
+
+lis = [6, 4, "Stroka", True, 6.5]
+print(lis[2:])
+print (lis[::2])
